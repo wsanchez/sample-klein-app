bb0de62a101d56d2688cdd1a73e990ded25df299
Wilfredo Sánchez <wsanchez@wsanchez.net>
2016-11-15 14:17:25 -0800

    Check for redirect for sub-app paths without trailing '/'

--- src/sample_klein_app/application/test/mock_render.py
+++ src/sample_klein_app/application/test/mock_render.py
@@ -2,6 +2,7 @@
 Tools for simulating resource rendering.
 """
 
+from twisted.python.url import URL
 from twisted.internet.defer import inlineCallbacks
 from twisted.web import http
 
@@ -15,14 +16,33 @@ __all__ = [
 
 @inlineCallbacks
 def assertResponse(
-    test, application, request_path, response_data, response_code=http.OK,
+    test, application,
+    request_path,
+    response_data=None,
+    response_code=http.OK,
+    response_location_path=None,
 ):
     request = mock_request(request_path)
 
     yield render(application, request)
 
     test.assertEqual(request.code, response_code)
-    test.assertEqual(request.getWrittenData(), response_data)
+
+    if response_data is not None:
+        data = request.getWrittenData()
+        test.assertEqual(data, response_data)
+
+    if response_location_path is not None:
+        values = request.responseHeaders.getRawHeaders("location")
+        test.assertTrue(
+            len(values) > 0, "No location header in response."
+        )
+        test.assertTrue(
+            len(values) == 1, "Too many location headers in response."
+        )
+        url = URL.fromText(values[0])
+        path = ("/" + "/".join(url.path)).encode("utf-8")
+        test.assertEqual(path, response_location_path)
 
 
 def render(app, request, notifyFinish=True):
--- src/sample_klein_app/application/test/test_composite.py
+++ src/sample_klein_app/application/test/test_composite.py
@@ -2,6 +2,8 @@
 Tests for L{sample_klein_app.application.composite}.
 """
 
+from twisted.internet.defer import inlineCallbacks
+from twisted.web import http
 from twisted.trial import unittest
 
 from .mock_render import assertResponse
@@ -21,6 +23,19 @@ class CompositeApplicationTests(unittest.TestCase):
         application = Application()
         return assertResponse(self, application, *args, **kwargs)
 
+    @inlineCallbacks
+    def assertSubApplication(self, sub_app, *args, **kwargs):
+        path_short = b"/" + sub_app
+        path_full = path_short + b"/"
+
+        yield self.assertResponse(
+            path_short,
+            response_code=http.MOVED_PERMANENTLY,
+            response_location_path=path_full,
+        )
+
+        yield self.assertResponse(path_full, *args, **kwargs)
+
     def test_root(self):
         """
         L{Application.root} returns a canned string.
@@ -34,12 +49,12 @@ class CompositeApplicationTests(unittest.TestCase):
         )
 
     def test_dns(self):
-        return self.assertResponse(b"/dns/", response_data=b"DNS API.")
+        return self.assertSubApplication(b"dns", response_data=b"DNS API.")
 
     def test_hello(self):
-        return self.assertResponse(b"/hello/", response_data=b"Hello!")
+        return self.assertSubApplication(b"hello", response_data=b"Hello!")
 
     def test_math(self):
-        return self.assertResponse(
-            b"/math/", response_data=b"Math happens here.",
+        return self.assertSubApplication(
+            b"math", response_data=b"Math happens here."
         )
