353e087f9ca633f261ab222bff3684cae0747cbf
Wilfredo Sánchez Vega <wsanchez@apple.com>
2016-12-16 13:39:24 -0800

    Add docstring.

--- src/sample_klein_app/application/test/async.py
+++ src/sample_klein_app/application/test/async.py
@@ -17,6 +17,15 @@ __all__ = (
 # Ideally, we want trial to handle coroutines natively, so we won't need this
 # decorator.
 def defer_async(f: Callable) -> Callable:
+    """
+    Wrap a coroutine such that it returns a deferred.
+
+    This is useful when you want to write a coroutine (an C{async def}
+    callable) that is going to be called by code that expects a callable that
+    returns a deferred.
+
+    @param f: A coroutine.
+    """
     @wraps(f)
     def wrapper(*args, **kwargs) -> Deferred:
         result = f(*args, **kwargs)
