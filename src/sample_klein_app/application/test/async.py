33ece1dc4b0e5a7cf417c72cf21ef16f315347fd
Wilfredo Sánchez Vega <wsanchez@apple.com>
2017-01-20 15:30:43 -0800

    Doc

--- src/sample_klein_app/application/test/async.py
+++ src/sample_klein_app/application/test/async.py
@@ -3,7 +3,6 @@ Async decorator for trial
 """
 
 from typing import Callable
-
 from functools import wraps
 
 from twisted.internet.defer import Deferred, ensureDeferred
@@ -18,13 +17,9 @@ __all__ = (
 # decorator.
 def defer_async(f: Callable) -> Callable:
     """
-    Wrap a coroutine such that it returns a deferred.
+    Wrap an asynchronous callable with a callable that returns a L{Deferred}.
 
-    This is useful when you want to write a coroutine (an C{async def}
-    callable) that is going to be called by code that expects a callable that
-    returns a deferred.
-
-    @param f: A coroutine.
+    @param f: An asynchronous callable.
     """
     @wraps(f)
     def wrapper(*args, **kwargs) -> Deferred:
